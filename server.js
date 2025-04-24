70f8b453e07792d71dbf9634fd3cbe97f5ff3e61
BEA <124007083+BrayanEAA@users.noreply.github.com>
2025-04-24 00:31:09 -0600

    Primer commit con la estructura del proyecto

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const app = require('./src/app');
+const connectDB = require('./src/services/db.config');
+
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`La aplicación esta escuchando en http://localhost:${PORT}`);
+});
